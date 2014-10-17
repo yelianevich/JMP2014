a59353c01b428b295bb7798ebd48c0f5182ab0a5
Raman Yelianevich <raman.yelianevich@gmail.com>
2014-10-17 03:31:53 -0300

    [Q&A] static and instance logical blocks demo

---
+++ src/main/java/com/epam/jmp/memory/questions/ClassWithStatic.java
@@ -0,0 +1,33 @@
+package com.epam.jmp.memory.questions;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ClassWithStatic {
+	private static final Logger LOG = LogManager.getLogger(ClassWithStatic.class);
+	private int instInt = 1;
+	private static int staticInt = 1;
+
+	{
+		LOG.info("instInt before first logical block = " + instInt);
+		LOG.info("logical block 1");
+		instInt = 2;
+	}
+
+	public ClassWithStatic() {
+		LOG.info("Constructor");
+		LOG.info("instInt = " + instInt);
+		LOG.info("staticInt = " + staticInt);
+	}
+
+	{
+		LOG.info("logical block 2");
+		instInt = 3;
+	}
+
+	static {
+		LOG.info("staticInt before static logical block = " + staticInt);
+		LOG.info("static logical block 2");
+		staticInt = 999;
+	}
+}
---
+++ src/main/java/com/epam/jmp/memory/questions/Test.java
@@ -0,0 +1,9 @@
+package com.epam.jmp.memory.questions;
+
+public final class Test {
+
+	public static void main(String[] args) {
+		ClassWithStatic classWithStatic = new ClassWithStatic();
+	}
+
+}
