2efa8841d82189f81290c64d1ffe2ec4e39307b5
Raman Yelianevich <raman.yelianevich@gmail.com>
2014-10-29 12:11:06 -0300

    [Concurrency] initial version of a job to read folder added

---
+++ src/main/java/com/epam/jmp/concurrency/impl/ImportJob.java
@@ -0,0 +1,17 @@
+package com.epam.jmp.concurrency.impl;
+
+import com.epam.jmp.util.Config;
+import static com.epam.jmp.util.Config.INPUT_FOLDER;;
+
+public class ImportJob implements Runnable {
+
+	@Override
+	public void run() {
+		String dir = Config.INST.prop(INPUT_FOLDER);
+		FolderReader reader = new FolderReader(dir);
+		ReaderJob readerJob = new ReaderJob(reader);
+		readerJob.run();
+
+	}
+
+}
---
+++ src/main/java/com/epam/jmp/concurrency/impl/ReaderJob.java
@@ -0,0 +1,35 @@
+package com.epam.jmp.concurrency.impl;
+
+import static com.epam.jmp.util.Config.READ_TIMEOUT;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.epam.jmp.util.Config;
+
+public class ReaderJob implements Runnable {
+	private static final Logger LOG = LogManager.getLogger(ReaderJob.class);
+	private final ScheduledExecutorService reader;
+	private final FilesReader filesReader;
+
+	public ReaderJob(FilesReader filesReader) {
+		this.filesReader = filesReader;
+		reader = Executors.newSingleThreadScheduledExecutor(run -> {
+			Thread t = new Thread(run, "TransactionReader");
+			t.setDaemon(true);
+			return t;
+		});
+	}
+
+	@Override
+	public void run() {
+		int readTimeout = Integer.parseInt(Config.INST.prop(READ_TIMEOUT));
+		reader.scheduleWithFixedDelay(() -> filesReader.readFiles(), readTimeout, readTimeout,
+				TimeUnit.SECONDS);
+	}
+
+}
