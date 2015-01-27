794358e8cf92792f2e2cac1887e209381105f174
Raman Yelianevich <raman.yelianevich@gmail.com>
2015-01-27 23:49:00 +0300

    combine directives with controller logic

--- spring-mvc/src/main/resources/static/js/app.js
+++ spring-mvc/src/main/resources/static/js/app.js
@@ -16,100 +16,93 @@
 
 		this.fetchEmployees();
 	});
-	
-	app.controller('EmployeeController', function($http) {
-		this.isEdit = false;
 
-		this.edit = function() {
-			this.isEdit = true;
-		};
-
-		this.cancel = function(employeeEdit, isDirty) {
-			isDirty && $http.get('/employees/' + employeeEdit.id)
-				.success(function(employeeSaved) {
-					var dateObj = new Date(employeeSaved.started);
-					employeeSaved.started = dateObj;
-					angular.copy(employeeSaved, employeeEdit);
-				});
-		};
-
-		this.save = function(employeeEdit) {
-			$http.put('/employees', employeeEdit)
-				.error(function(){
-					alert("Could not save employee :(");
-				});
-			this.isEdit = false;
-		};
-
-		this.delete = function(employee) {
-			this.isEdit = false;
-			$http.delete('/employees/' + employee.id)
-				.error(function(){
-					alert("Could not delete employee :(");
-				});
-		};
-
-		this.editing = function() {
-			return this.isEdit === true;
-		};
-
-		this.viewing = function() {
-			return this.isEdit === false;
-		};
-	});
-
-	app.controller('AddEmployeeController', function($http) {
-		this.employee = {};
-		this.isEdit = false;
-
-		this.edit = function() {
-			this.isEdit = true;
-		};
-
-		this.cancel = function() {
-			this.employee = {};
-			this.isEdit = false;
-		};
-
-		this.editing = function() {
-			return this.isEdit === true;
-		};
-
-		this.viewing = function() {
-			return this.isEdit === false;
-		};
-		
-		this.addEmployee = function() {
-			var self = this;
-			$http.post('/employees', this.employee)
-				.error(function() {
-					alert("Could not save new employee :(");
-				})
-				.success(function(){
-					self.isEdit = false;
-					self.employee = {};
-				});
-		};
-	});
-
-	app.directive('employeeView', function(){
+	app.directive('employeeViewEdit', function(){
 		return {
 			restrict: 'E',
-			templateUrl: 'template/employee-view.html'
+			templateUrl: 'template/employee-view-edit.html',
+			controllerAs: 'emplForm',
+			controller: function($http) {
+				this.isEdit = false;
+
+				this.edit = function() {
+					this.isEdit = true;
+				};
+
+				this.cancel = function(employeeEdit, isDirty) {
+					isDirty && $http.get('/employees/' + employeeEdit.id)
+						.success(function(employeeSaved) {
+							var dateObj = new Date(employeeSaved.started);
+							employeeSaved.started = dateObj;
+							angular.copy(employeeSaved, employeeEdit);
+						});
+				};
+
+				this.save = function(employeeEdit) {
+					$http.put('/employees', employeeEdit)
+						.error(function(){
+							alert("Could not save employee :(");
+						});
+					this.isEdit = false;
+				};
+
+				this.delete = function(employee) {
+					this.isEdit = false;
+					$http.delete('/employees/' + employee.id)
+						.error(function(){
+							alert("Could not delete employee :(");
+						});
+				};
+
+				this.editing = function() {
+					return this.isEdit === true;
+				};
+
+				this.viewing = function() {
+					return this.isEdit === false;
+				};
+			}
 		};
 	});
 
-	app.directive('employeeEdit', function(){
+	app.directive('employeeAdd', function() {
 		return {
 			restrict: 'E',
-			templateUrl: 'template/employee-edit.html'
-		};
-	});
+			templateUrl: 'template/employee-add.html',
+			controllerAs: 'addEmployeeCtrl',
+			controller: function($http) {
+				this.employee = {};
+				this.isEdit = false;
 
-	app.directive('employeeAdd', function(){
-		return {
-			restrict: 'E',
-			templateUrl: 'template/employee-add.html'
+				this.edit = function() {
+					this.isEdit = true;
+				};
+
+				this.cancel = function() {
+					this.employee = {};
+					this.isEdit = false;
+				};
+
+				this.editing = function() {
+					return this.isEdit === true;
+				};
+
+				this.viewing = function() {
+					return this.isEdit === false;
+				};
+				
+				this.addEmployee = function() {
+					var self = this;
+					$http.post('/employees', this.employee)
+						.error(function() {
+							alert("Could not save new employee :(");
+						})
+						.success(function(){
+							self.isEdit = false;
+							self.employee = {};
+						});
+				};
+			}
 		};
 	});
 
