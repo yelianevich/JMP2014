593249fdb16ef30b5f8fad96bac5f3b2305f30a2
Raman Yelianevich <raman.yelianevich@gmail.com>
2015-01-28 07:08:17 -0300

    JMS task using RabbitMQ

---
+++ jms/src/main/java/com/epam/rabbitmq/helloworld/Receive.java
@@ -0,0 +1,35 @@
+package com.epam.rabbitmq.helloworld;
+
+import java.io.IOException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.ConsumerCancelledException;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.ShutdownSignalException;
+
+public class Receive {
+
+	private final static String QUEUE_NAME = "hello";
+
+	public static void main(String[] args) throws IOException, ShutdownSignalException, ConsumerCancelledException, InterruptedException {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+		QueueingConsumer consumer = new QueueingConsumer(channel);
+		channel.basicConsume(QUEUE_NAME, true, consumer);
+
+		while (true) {
+			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+			String message = new String(delivery.getBody());
+			System.out.println(" [x] Received '" + message + "'");
+		}
+	}
+
+}
