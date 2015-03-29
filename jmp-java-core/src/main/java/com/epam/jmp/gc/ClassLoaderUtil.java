0ff3b087538da9648b86798abf9acbfa3498ba91
Raman Yelianevich <raman.yelianevich@gmail.com>
2015-03-29 19:55:47 +0300

    Fix FindBugs violations

--- jmp-java-core/src/main/java/com/epam/jmp/gc/ClassLoaderUtil.java
+++ jmp-java-core/src/main/java/com/epam/jmp/gc/ClassLoaderUtil.java
@@ -4,18 +4,17 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-/**
- * User: Ivan_Spresov Date: 3/7/14
- */
 public class ClassLoaderUtil {
 	public static byte[] loadByteCode(Class<?> clazz, String className) throws IOException {
 		String fileName = "/" + className.replaceAll("\\.", "/") + ".class";
-		InputStream is = clazz.getResourceAsStream(fileName);
-		ByteArrayOutputStream buffer = new ByteArrayOutputStream(4096);
-		byte[] buff = new byte[1024];
-		int i;
-		while ((i = is.read(buff)) >= 0) {
-			buffer.write(buff, 0, i);
+		ByteArrayOutputStream buffer = null;
+		try (InputStream is = clazz.getResourceAsStream(fileName)) {
+			buffer = new ByteArrayOutputStream(4096);
+			byte[] buff = new byte[1024];
+			int i;
+			while ((i = is.read(buff)) >= 0) {
+				buffer.write(buff, 0, i);
+			}
 		}
 
 		return buffer.toByteArray();
